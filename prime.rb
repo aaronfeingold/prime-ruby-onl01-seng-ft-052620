eca2ef148bbd203d7ce3ef8ec678b1e46dd79e64
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-02 23:13:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 # Add  code here!
 
-def is_prime?(n)
-  return false if n < 2
-
-  (2..n/2).none?{|i| n % i == 0}
+def is_prime? (n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+    end
 end
