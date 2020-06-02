40c6a020dacc2d883df7c6b55ec4ef2e55b2a7f4
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-02 23:10:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,26 +1,6 @@
 # Add  code here!
 
-#def prime?(num)
-#  prime_numbers = []
-#  num.each do |number|
-#      prime_numbers << number if is_prime(number)
-#  end
-#  prime_numbers
-# end
-
-# def is_prime(n)
-#   if factors(n).count > 2
-#      return true
-#   end
-#   return false
-# end
-
-def is_prime? (n) 
-  2.upto(Math.sqrt(n)) do |x| 
-    if n % x == 0
-      return false # this means the number is not prime
-    else
-      return true # this means the number is prime
-    end 
-  end
+def is_prime?(num)
+  return if num <= 1
+  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
 end
