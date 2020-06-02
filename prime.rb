76af3723381e4d4a46e262450e4b24150b9e197f
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-02 23:02:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 
-def prime?(num)
-  prime_numbers = []
-  num.each do |number|
-      prime_numbers << number if is_prime(number)
-  end
-  prime_numbers
-end
+#def prime?(num)
+#  prime_numbers = []
+#  num.each do |number|
+#      prime_numbers << number if is_prime(number)
+#  end
+#  prime_numbers
+# end
 
 # def is_prime(n)
 #   if factors(n).count > 2
