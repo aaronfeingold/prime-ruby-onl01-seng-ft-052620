a3e2670332d5d89cf1fc8a5152643f87391fee2e
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-02 22:39:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+def prime?(num)
+  return true if num == 2
+  return false if num <= 1 || num.even?
+  i = 3
+  top = Math.sqrt(num).floor
+  loop do
+    return false if (num % i).zero?
+    i += 2
+    break if i >= top
+  end
+  true
+end
